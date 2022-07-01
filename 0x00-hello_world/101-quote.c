802411bc73a00526e9a8399ce46784e86a25db32
phatlines <muyiwafatunsin@gmail.com>
2022-07-01 14:30:19 -0700

    Task 8- UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print("and that piece of art is useful\"-Dora Korpar,2015-10-19\n");
-	return (1);
+char str[] = "and that piece of art is useful\"-Dora Korpar,2015-10-19\n);
+write(2, str, 59);
+
+return (1);
 }
