dce1a50052276a09055e97c842b8bb96bfe0bfc4
phatlines <muyiwafatunsin@gmail.com>
2022-07-01 00:51:59 -0700

    Task 4- Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #!/bin/bash
-# include <stdio.h>
-   /**
-    * main - entry point
-    * Return; 0 after printing the function
-    */
-int main (void)
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
 {puts ("\"Programming is like building a multilingual puzzle");
-  return (o);
-}
+ return (0)
+ }
