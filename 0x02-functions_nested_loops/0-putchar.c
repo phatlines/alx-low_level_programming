beddabab43b7ae8b004800be1c089a67f30a0420
phatlines <muyiwafatunsin@gmail.com>
2022-07-05 13:39:08 -0700

    Task 0- _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *  Return: Always 0
+ * Return: Always 0
  */
 int main(void)
 {
-        _putchar('m');
-        _putchar('a');
-        _putchar('i');
-        _putchar('n');
-        _putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-        Return(0);
+	return (0);
 }
