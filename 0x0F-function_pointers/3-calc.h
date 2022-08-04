943f5e47e22a6686d2a20b85f6fc05316e6dfcf1
phatlines <muyiwafatunsin@gmail.com>
2022-08-04 06:59:03 -0700

    Fixed Task-3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,12 +1,14 @@
-#ifndef _CALC_H
-#define _CALC_H
-
+#ifndef CALC_H
+#define CALC_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
-*/
+ */
 typedef struct op
 {
 	char *op;
@@ -19,5 +21,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
-#endif /* _CALC_H */
+#endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,57 +1,36 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <string.h> /*for atoi*/
-#include <stdlib.h> /*for exit*/
 
 /**
- * main - Program Entry
- *
- * Description: Program that takes 4 arguments where 2 are
- *              int variables plus an operator to perform
- *              a function between the two numbers
- *
- *              Usage: ./calc num1 operator num2
- *                         |     |       |     |_ argv[3]
- *                         |     |       |_ argv[2]
- *                         |     |_ argv[1]
- *                         |_ argv[0]
- *
- * @argc: argument counter
- * @argv: argument vector
- *
- * Return: 0-on success
-*/
+ * main - Entry point
+ * @argc: number of argument
+ * @argv: arrays of pointers to the argument
+ * Return: Always 0, else exit with the status 98, 99 or 100
+ * if the number of argument is wrong, if the operator does not match
+ * or if you try to divde (/ or %) by 0 respectively
+ */
+
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	char *operator;
+	int num1, num2, result;
 
-	/* Error if arguments are not 4 with the executable */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]); /*if argument is string convert to number*/
-	num2 = atoi(argv[3]); /* as stated above */
-	operator = argv[2];
-
-	/* Error if operator is not null or a 2d array '\*' */
-	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (!get_op_func(argv[2]))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	/* Error if user tries to divide / (47) or % (37) by 0*/
-	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	printf("%d\n", get_op_func(operator)(num1, num2));
-
+	result = (get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
